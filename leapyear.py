ea20ca4724907270c10230596c1cc9334159dbcd
David Gasper <gasperd@oregonstate.edu>
2021-03-05 14:24:33 -0800

    Implemented third method, test invalid year

--- leapyear.py
+++ leapyear.py
@@ -1,3 +1,6 @@
 def isLeapYear(input):
-	if (input % 4 == 0 and input % 100 != 0 and input % 400 != 0): return True
-	else: return False
+	try:
+		if (input % 4 == 0 and input % 100 != 0 and input % 400 != 0): return True
+		else: return False
+	except TypeError:
+		return None
