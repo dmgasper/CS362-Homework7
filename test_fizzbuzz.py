4d7cf5a64e4b5746a5106085c4f46bec4660c3e5
David Gasper <gasperd@oregonstate.edu>
2021-03-05 14:06:53 -0800

    Wrote second test, process multiples of 5

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -5,6 +5,10 @@ class TestListAverage(unittest.TestCase):
 	def test_fizz(self):
 		result = fizzbuzz.getOutput(3)
 		self.assertEqual(result, "Fizz")
+		
+	def test_buzz(self):
+		result = fizzbuzz.getOutput(5)
+		self.assertEqual(result, "Buzz")
 
 		
 if __name__ == "__main__":
