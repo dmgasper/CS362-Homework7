8b77f68fb8911b9598e3f9e63d621ecb89308bd4
David Gasper <gasperd@oregonstate.edu>
2021-03-05 14:21:00 -0800

    Wrote Second Test, test valid non-leap year

--- test_leapyear.py
+++ test_leapyear.py
@@ -7,5 +7,9 @@ class TestLeapYear(unittest.TestCase):
 		result = leapyear.isLeapYear(2012)
 		self.assertEqual(result, True)
 		
+	def test_isnotleapyear(self):
+		result = leapyear.isLeapYear(2019)
+		self.assertEqual(result, False)
+		
 if __name__ == "__main__":
 	unittest.main()
